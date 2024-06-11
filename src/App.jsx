41d56a2900f9ad93fec6c46ee8dd48dbcd16881f
Haileyesus Chekol <teamdevs7@gmail.com>
2024-06-11 21:07:49 +0100

    practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import { useFetch } from "./hook/useFetch"
 function App() {
-  return <>react app</>
+  const fData = useFetch(1)
+  const { id, name } = fData
+  return (
+    <>
+      <h1>
+        {id}, {name}
+      </h1>
+      <h1>Fetch</h1>
+    </>
+  )
 }
 
 export default App

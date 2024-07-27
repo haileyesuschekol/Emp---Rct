ca48c43ff7fff32acf60b210dd353cba55671b86
Haileyesus Chekol <teamdevs7@gmail.com>
2024-07-27 18:24:16 +0100

    add custom form

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,19 @@ const App = () => {
         count: state.count - 1,
       }
     }
+    if (action.type === "change") {
+      return {
+        ...state,
+        valueToAdd: action.payload,
+      }
+    }
+    if (action.type === "addValue") {
+      return {
+        ...state,
+        count: state.count + state.valueToAdd,
+        valueToAdd: 0,
+      }
+    }
   }
 
   const [state, despatch] = useReducer(reducer, {
@@ -32,11 +45,35 @@ const App = () => {
       type: "decrement",
     })
   }
+
+  const handleChange = (e) => {
+    const value = parseInt(e.target.value)
+    despatch({
+      type: "change",
+      payload: value,
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    despatch({
+      type: "addValue",
+    })
+  }
   return (
     <div>
       <p>{state.count}</p>
       <button onClick={increment}>Inc</button>
       <button onClick={decrement}>Decr</button>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="number"
+          value={state.valueToAdd || ""}
+          onChange={handleChange}
+        />
+        <button>Add</button>
+      </form>
     </div>
   )
 }
